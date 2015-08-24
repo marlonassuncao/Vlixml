0a0661ef3829d7790fdd8614fb7d21a234cb7b2e
Marlon Assunção <hi@marlonassuncao.com>
2015-08-24 15:30:04 -0300

    v1

--- Gulpfile.js
+++ Gulpfile.js
@@ -12,31 +12,46 @@ var gulp = require('gulp'),
     minifyCSS = require('gulp-minify-css'),
     rename = require("gulp-rename"),
     livereload = require('gulp-livereload'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    autoprefixer = require('gulp-autoprefixer');
 
-// less
+// sass
 
 gulp.task('sass', function() {
     gulp.src('./app/styles/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./app/styles'));
-});
-
-// css
-
-gulp.task('css', function() {
-    gulp.src('./app/styles/main.css')
-        .pipe(minifyCSS())
-        .pipe(rename("main.min.css"))
         .pipe(gulp.dest('./app/styles'))
+        .pipe(autoprefixer(
+            {
+                browsers: [
+                    '> 1%',
+                    'last 2 versions',
+                    'firefox >= 4',
+                    'safari 7',
+                    'safari 8',
+                    'IE 8',
+                    'IE 9',
+                    'IE 10',
+                    'IE 11'
+                ],
+                cascade: false
+            }
+        ))
+        .pipe(minifyCSS())
+        .pipe(rename('main.min.css'))
+        .pipe(gulp.dest('./app/styles'))
+        livereload.listen();
 });
+
+// default
+gulp.task('default', ['sass']);
+
 
 // watch
 
 gulp.task('watch', function() {
-    livereload.listen();
     gulp.watch('./app/styles/main.scss', ['sass']);
-    gulp.watch('./app/styles/main.css', ['css']);
     gulp.watch('*').on('change', livereload.changed);
     gulp.watch('./app/styles/main.min.css').on('change', livereload.changed);
+    livereload.listen();
 });
