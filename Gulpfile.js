25bba121427bfbc8fd6b3fb63914ac0df667fa70
Marlon Assunção <marlonassuncao@Marlons-Mac-mini.local>
2015-12-14 14:05:32 -0200

    v1.1

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,57 +1,64 @@
-/*
-    Gulp tasks.
-*/
+// Gulpfile
 
 'use strict';
 
 // load plugins
-
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     path = require('path'),
     minifyCSS = require('gulp-minify-css'),
     rename = require("gulp-rename"),
-    livereload = require('gulp-livereload'),
     watch = require('gulp-watch'),
-    autoprefixer = require('gulp-autoprefixer');
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer'),
+    php = require('gulp-connect-php');
+
+// default
+gulp.task('default', ['server']);
+
+// server
+gulp.task('server', ['sass'], function() {
+
+	// php server
+    php.server({}, function() {
+        browserSync({
+            proxy: '127.0.0.1:8000'
+        });
+    });
+
+    // html server
+    // browserSync.init({
+    //     server: "./"
+    // });
+	
+	// watch files
+    gulp.watch('./app/styles/main.scss', ['sass']);
+    gulp.watch('./app/styles/main.min.css', ['sass']);
+    gulp.watch('./app/scripts/main.min.js').on('change', browserSync.reload);
+    gulp.watch('./*').on('change', browserSync.reload);
+});
 
 // sass
-
 gulp.task('sass', function() {
     gulp.src('./app/styles/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./app/styles'))
-        .pipe(autoprefixer(
-            {
-                browsers: [
-                    '> 1%',
-                    'last 2 versions',
-                    'firefox >= 4',
-                    'safari 7',
-                    'safari 8',
-                    'IE 8',
-                    'IE 9',
-                    'IE 10',
-                    'IE 11'
-                ],
-                cascade: false
-            }
-        ))
+        .pipe(autoprefixer({
+            browsers: [
+                '> 1%',
+                'last 2 versions',
+                'firefox >= 4',
+                'safari 7',
+                'safari 8',
+                'IE 8',
+                'IE 9',
+                'IE 10',
+                'IE 11'
+            ],
+            cascade: false
+        }))
         .pipe(minifyCSS())
         .pipe(rename('main.min.css'))
         .pipe(gulp.dest('./app/styles'))
-        livereload.listen();
-});
-
-// default
-gulp.task('default', ['sass']);
-
-
-// watch
-
-gulp.task('watch', function() {
-    gulp.watch('./app/styles/main.scss', ['sass']);
-    gulp.watch('*').on('change', livereload.changed);
-    gulp.watch('./app/styles/main.min.css').on('change', livereload.changed);
-    livereload.listen();
+        .pipe(browserSync.stream());
 });
--- index.php
+++ index.php
@@ -1,54 +1,41 @@
 <!doctype html>
 <html class="no-js" lang="pt">
     <head>
-
-    	<!-- meta tags -->
+        <!-- meta tags -->
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Vlixml</title>
-
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- end meta tags -->
-        
         <!-- favicon -->
-        <link rel="apple-touch-icon" href="app/images/apple-touch-icon.png">
+        <link rel="icon" href="app/images/favicon.png" />
         <!-- end favicon -->
-
-
         <!-- styles -->
         <link rel="stylesheet" href="app/styles/bootstrap.min.css">
         <link rel="stylesheet" href="app/styles/main.min.css">
         <!-- end styles -->
-		
-		<!-- modernizr -->
+        <!-- modernizr -->
         <script src="app/scripts/vendor/modernizr.min.js"></script>
         <!-- end modernizr -->
-
     </head>
     <body>
-
-    <!--[if lt IE 8]>
-        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-    <![endif]-->
-		
-		
-		<!-- scripts -->
-		<script src="app/scripts/vendor/jquery.min.js"></script>
-        <script src="app/scripts/vendor/bootstrap.min.js"></script>
+        
+        <p>Hello World!</p>
+        
+        <!-- scripts -->
+        <script src="app/scripts/vendor/jquery.min.js"></script>
         <script src="app/scripts/main.min.js"></script>
         <!-- end scripts -->
-
         <!-- analytics -->
         <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='//www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','UA-XXXXX-X','auto');ga('send','pageview');
         </script>
         <!-- end analytics -->
-
     </body>
 </html>
